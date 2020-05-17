d8f8ab4cd31179fc06f4ac71b740895def537cd2
Thiago Canete <thiago.canete@gmail.com>
2020-05-17 09:40:57 -0300

    ajustes

--- EnrollmentSystem/Domain/Turmas/Turma.cs
+++ EnrollmentSystem/Domain/Turmas/Turma.cs
@@ -6,8 +6,7 @@ namespace EnrollmentSystem.Domain.Turmas
     public class Turma
     {
         public Guid Id { get; set; }
-        public string Nome { get; set; }
-        
+        public string Nome { get; set; }        
         public IList<Aluno> Alunos { get; set; }
     }
 }
